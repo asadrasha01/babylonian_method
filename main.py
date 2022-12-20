1c7cb64c49b2c6d2ccee58284dbb9de1c8f1206e
asadrasha01 <74987517+asadrasha01@users.noreply.github.com>
2022-12-20 18:07:13 +0200

    name change

--- main.py
+++ main.py
@@ -1,9 +1,16 @@
 def main(S, d):
+    '''create a babylonian function.
     
-    a = (S - d**2)/(2*d)
+    Args:
+        S (int): number
+        d (int): numnber
+        
+    Returns:
+        float: result
+    '''
+    a = (S - d**2) / (2*d)
     b = a + d
     x = b - (a**2) / (2*b)
-    return x
-
-result = main (10, 2)
+    return float(x)
+result = main(16, 4)
 print(result)
